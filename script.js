881a96f9d5306eeec21235e8c98bfbaf5deb537c
Jordi Pujol Ricarte <jpricarte@inf.ufrgs.br>
2020-02-17 17:02:18 -0300

    Implemeting pause button

--- script.js
+++ script.js
@@ -1,46 +1,73 @@
 var endTime = new Date().getTime();
 var endDate = new Date(endTime);
+
 var hour = endDate.getHours();
 var min = endDate.getMinutes();
 var sec = endDate.getSeconds();
+
 document.getElementById('endTime').innerHTML = hour.toString()+'h '+min.toString()+'m '+sec.toString()+'s';
 
 var counter;
+var isRunning = false;
+var remainTime = 1500000;
+var distance = remainTime;
 
 function setTimer()
 {
-    endTime = new Date().getTime()+1500000;
+    endTime = new Date().getTime() + remainTime;
     endDate = new Date(endTime);
+
     hour = endDate.getHours();
     min = endDate.getMinutes();
     sec = endDate.getSeconds();
+
     document.getElementById('endTime').innerHTML = hour.toString()+'h '+min.toString()+'m '+sec.toString()+'s';
-    counter = setInterval(() => {
+    
+    if(isRunning == false)
+    {
+        endTime = new Date().getTime() + remainTime;
+        isRunning = true;
+        document.getElementById('start').innerText = 'Pause timer';
+    }
+    else
+    {
+        clearInterval(counter);
+        isRunning = false;
+        remainTime = distance;
+        document.getElementById('start').innerText = 'Start timer';
+    }
 
-        var nowTime = new Date();
-        var distance = endTime - nowTime;
+    if(isRunning)
+    {
+        counter = setInterval(() => {
+            var nowTime = new Date();
+            distance = endTime - nowTime;
+            console.log(distance);
+            var restMin = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var restSec = Math.floor((distance % (1000 * 60)) / 1000);
+            document.getElementById('time').innerHTML = restMin.toString()+'m '+restSec.toString()+'s';
 
-        var restMin = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var restSec = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById('time').innerHTML = restMin.toString()+'m '+restSec.toString()+'s';
-        console.log(nowTime);
-
-        if(distance < 0)
-        {
-            clearInterval(counter);
-            alert('Time ended! Go get some coffee, dude!')
-        }
-    },1000)
+            if(distance < 0)
+            {
+                clearInterval(counter);
+                alert('Time ended! Go get some coffee, dude!')
+            }
+        },1000);
+    }
 }
 
 function resetTimer()
 {
     clearInterval(counter);
+    isRunning = false;
+
     var endTime = new Date().getTime();
     var endDate = new Date(endTime);
+
     var hour = endDate.getHours();
     var min = endDate.getMinutes();
     var sec = endDate.getSeconds();
+
     document.getElementById('endTime').innerHTML = hour.toString()+'h '+min.toString()+'m '+sec.toString()+'s';
     document.getElementById('time').innerHTML = '25m 00s';
 }
